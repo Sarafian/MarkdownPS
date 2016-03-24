3d6bdd5ebab2851cc2b14849c99a9d7390186149
a.sarafian <dev1978@outlook.com>
2016-03-24 23:42:36 +0100

    Another attempt

--- Pester/Initialize-Dependencies.ps1
+++ Pester/Initialize-Dependencies.ps1
@@ -1,11 +1,16 @@
 # When ConvertTo-Markdown commandlet is not available
 # Download and import
 if(-not (Get-Command ConvertTo-Markdown -ErrorAction SilentlyContinue)) {
+    Write-Warning "ConvertTo-Markdown commandlet not found"
+    $modulePath= Resolve-Path "$PSScriptRoot\Modules"
     $url='https://raw.githubusercontent.com/ishu3101/PSMarkdown/master/ConvertTo-Markdown.ps1'
     $output = Join-Path $env:TEMP "ConvertTo-Markdown.ps1"
+    Write-Verbose "Downloading $url to $output"
 
     $wc = New-Object System.Net.WebClient
     $wc.DownloadFile($url, $output)
+
+    Write-Verbose "Importing $output"
 
     . $output
 }
