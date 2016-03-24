cbae06067cf7d9051c89ba12f5c68a76c583784e
a.sarafian <dev1978@outlook.com>
2016-03-24 23:17:29 +0100

    Fixed bug in Initialize-Pester.ps1.

--- Pester/Initialize-Pester.ps1
+++ Pester/Initialize-Pester.ps1
@@ -1,5 +1,5 @@
 #Check if Pester module is available
-if(-not (Get-Command New-Fixture)) {
+if(-not (Get-Command New-Fixture -ErrorAction SilentlyContinue)) {
     $modulePath= Resolve-Path "$PSScriptRoot\Modules"
     Write-Verbose "Add Pester modules directory to PSModulePath"
     $env:PSModulePath+=";$modulePath"
