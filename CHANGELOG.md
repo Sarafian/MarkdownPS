ae5863b6cfcdbd8fe4422e543d3bcc553cc29dfe
a.sarafian <dev1978@outlook.com>
2016-03-28 16:03:55 +0200

    Added a mock to ConvertTo-Markdown for the New-MDTable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+**20160328**
+- Mocked the ConvertTo-Markdown when the dependency is not found.
+
+
 **20160324**
 Initial commit with the following commandlets (No `Get-Help` support yet)
 
--- Modules/MarkdownPS/New-MDTable.Tests.ps1
+++ Modules/MarkdownPS/New-MDTable.Tests.ps1
@@ -1,7 +1,7 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
-
+$VerbosePreference="SilentlyContinue"
 $newLine=[System.Environment]::NewLine
 Describe "New-MDTable" {
     It "-Object is null" {
@@ -9,15 +9,42 @@ Describe "New-MDTable" {
     }
 }
 Describe "New-MDTable" {
-    $object=Get-Command New-MDTable |Select-Object Name,CommandType
-    It "-NoNewLine not specified" {
-        $expected=4
-        ((New-MDTable -Object $object) -split [System.Environment]::NewLine ).Length| Should Be $expected
-        (($object | New-MDTable) -split [System.Environment]::NewLine ).Length| Should Be $expected
-    }
-    It "-NoNewLine specified" {
-        $expected=3
-        ((New-MDTable -Object $object -NoNewLine) -split [System.Environment]::NewLine ).Length| Should Be $expected
-        (($object | New-MDTable -NoNewLine) -split [System.Environment]::NewLine ).Length| Should Be $expected
+    Context "Mock if necessary the ConvertTo-Markdown" {
+        $command=Get-Command ConvertTo-Markdown -ErrorAction SilentlyContinue
+        if((-not $command) -or $command.ModuleName -ne "PSMarkdown")
+        {
+            Write-Warning "Creating mock module PSMarkdownMock with ConvertTo-Markdown"
+            New-Module -Name PSMarkdownMock  -ScriptBlock {
+                function ConvertTo-Markdown { 
+                    Begin {
+                        $output=@(
+                            "Header"
+                            "Seperator"
+                        )
+                    }
+
+                    Process {
+                        $output+="Line"
+                    }
+
+                    End {
+                        $output
+                    }
+                }
+
+                Export-ModuleMember -Function ConvertTo-Markdown
+            } | Import-Module -Force
+        }
+        $object=Get-Command New-MDTable |Select-Object Name,CommandType
+        It "-NoNewLine not specified" {
+            $expected=4
+            ((New-MDTable -Object $object) -split [System.Environment]::NewLine ).Length| Should Be $expected
+            (($object | New-MDTable) -split [System.Environment]::NewLine ).Length| Should Be $expected
+        }
+        It "-NoNewLine specified" {
+            $expected=3
+            ((New-MDTable -Object $object -NoNewLine) -split [System.Environment]::NewLine ).Length| Should Be $expected
+            (($object | New-MDTable -NoNewLine) -split [System.Environment]::NewLine ).Length| Should Be $expected
+        }
     }
 }
