6c706901f38126d8cf38c37c05dc125bee3fd25e
Kröger Janek <janek.kroeger@acp.de>
2019-10-21 11:11:49 +0200

    Added tests for ordered hashtable as input

--- Modules/MarkdownPS/New-MDTable.Tests.ps1
+++ Modules/MarkdownPS/New-MDTable.Tests.ps1
@@ -45,6 +45,38 @@ Describe "New-MDTable with columns" {
     }
 }
 
+Describe "New-MDTable with ordered hashtable and without columns" {
+    $object=[PSCustomObject]@{
+        Name = "This should be the first value"
+        ZProperty = "This should be in the middle"
+        AnotherProperty = "This should be the last value"
+    }
+    It "-NoNewLine not specified" {
+        $expected=4
+        ((New-MDTable -Object $object) -split [System.Environment]::NewLine).Length | Should Be $expected
+        (($object | New-MDTable) -split [System.Environment]::NewLine).Length | Should Be $expected
+        ((@($object, $object) | New-MDTable) -split [System.Environment]::NewLine).Length | Should Be ($expected+1)
+    }
+    It "-NoNewLine not specified" {
+        $expected=3
+        ((New-MDTable -Object $object -NoNewLine) -split [System.Environment]::NewLine).Length | Should Be $expected
+        (($object | New-MDTable -NoNewLine) -split [System.Environment]::NewLine).Length | Should Be $expected
+        ((@($object, $object) | New-MDTable -NoNewLine) -split [System.Environment]::NewLine).Length | Should Be ($expected+1)
+    }
+    It "Header should be in correct order" {
+        $HeaderRegex = "^\|\s([\w\d]+)\s*\|\s([\w\d]+)\s*\|\s([\w\d]+)\s*\|$"
+        $expectedHeader = ($object.PsObject.Members | Where-Object {$_.MemberType -eq "NoteProperty"})[0..2].Name
+        (((New-MDTable -Object $object) -split [System.Environment]::NewLine)[0]) -match $HeaderRegex
+        $Matches[1..3] | Should Be $expectedHeader
+
+        ((($object | New-MDTable) -split [System.Environment]::NewLine)[0]) -match $HeaderRegex
+        $Matches[1..3] | Should Be $expectedHeader
+
+        (((@($object, $object) | New-MDTable) -split [System.Environment]::NewLine)[0]) -match $HeaderRegex
+        $Matches[1..3] | Should Be $expectedHeader
+    }
+}
+
 Describe "New-MDTable with ordered columns" {
     $object=Get-Command New-MDTable 
     $columns=[ordered]@{
